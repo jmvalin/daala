8caa0bd73ad15548c72c4a85bde88e2dfc1a571c
Jean-Marc Valin <jmvalin@jmvalin.ca>
2015-09-05 01:25:27 -0400

    7x7 filter

--- src/filter.c
+++ src/filter.c
@@ -1666,10 +1666,10 @@ void od_dering(od_coeff *y, int ystride, od_coeff *x, int xstride, int ln,
   n = 1 << ln;
   left = top = 0;
   right = bottom = n;
-  if (sbx == 0) left = 2;
-  if (sby == 0) top = 2;
-  if (sbx == nhsb - 1) right -= 2;
-  if (sby == nvsb - 1) bottom -= 2;
+  if (sbx == 0) left = 3;
+  if (sby == 0) top = 3;
+  if (sbx == nhsb - 1) right -= 3;
+  if (sby == nvsb - 1) bottom -= 3;
   if (sbx == 0 || sby == 0 || sbx == nhsb - 1 || sby == nvsb - 1) {
     for (i = 0; i < n; i++) {
       for (j = 0; j < n; j++) {
@@ -1687,8 +1687,8 @@ void od_dering(od_coeff *y, int ystride, od_coeff *x, int xstride, int ln,
       int count;
       xx = x[i*xstride + j];
       sum = count = 0;
-      for (k = -1; k <= 1; k++) {
-        for (m = -1; m <= 1; m++) {
+      for (k = -3; k <= 3; k++) {
+        for (m = -3; m <= 3; m++) {
           od_coeff yy;
           yy = x[(i + k)*xstride + j + m];
           if (abs(yy - xx) <= threshold) {
