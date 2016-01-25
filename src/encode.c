8ea6618b79eb48e280c176c87ed8a4091a647b78
Jean-Marc Valin <jmvalin@jmvalin.ca>
2016-01-25 16:38:32 -0500

    DC QM

--- src/encode.c
+++ src/encode.c
@@ -1534,8 +1534,8 @@ static void od_quantize_haar_dc_level(daala_enc_ctx *enc, od_mb_enc_ctx *ctx,
   }
   if (OD_LOSSLESS(enc, pli)) ac_quant[0] = ac_quant[1] = 1;
   else {
-    ac_quant[0] = (dc_quant*OD_DC_QM[bsi - xdec][0] + 8) >> 4;
-    ac_quant[1] = (dc_quant*OD_DC_QM[bsi - xdec][1] + 8) >> 4;
+    ac_quant[0] = (dc_quant*OD_DC_QM[xdec][bsi - xdec][0] + 8) >> 4;
+    ac_quant[1] = (dc_quant*OD_DC_QM[xdec][bsi - xdec][1] + 8) >> 4;
   }
   ln = bsi - xdec + 2;
   x[0] = ctx->d[pli][(by << ln)*w + (bx << ln)];
