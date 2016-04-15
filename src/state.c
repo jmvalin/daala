e8ee659ef50401891643fed894ab5693c78480a0
Jean-Marc Valin <jmvalin@jmvalin.ca>
2016-04-15 13:32:23 -0400

    rate=4

--- src/state.c
+++ src/state.c
@@ -537,7 +537,7 @@ void od_adapt_ctx_reset(od_adapt_ctx *state, int is_keyframe) {
   od_adapt_pvq_ctx_reset(&state->pvq, is_keyframe);
   generic_model_init(&state->mv_model);
   OD_CDFS_INIT(state->mv_ref_cdf, 128);
-  state->skip_rate = 7;
+  state->skip_rate = 4;
   OD_CLEARALL(state->skip_count);
   OD_CDFS_INIT(state->skip_cdf, 32);
   state->mv_small_increment = 128;
