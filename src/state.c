acd7500ff2ef9334a07e45d025966bf6d941698a
Jean-Marc Valin <jmvalin@jmvalin.ca>
2016-04-15 03:37:14 -0400

    Converts skip/split symbols to q15 adaptation
    subset1 results:
              LOW (%)  MEDIUM (%) HIGH (%)
        PSNR -0.046886 -0.103715 -0.107475
     PSNRHVS -0.001031 0.008097 0.006270
        SSIM 0.073147 0.002298 -0.029763
    FASTSSIM 0.257397 0.081080 0.030405
    
    ntt-short1 results:
              LOW (%)  MEDIUM (%) HIGH (%)
        PSNR 0.113471 0.023280 -0.069023
     PSNRHVS 0.151475 0.124430 0.135461
        SSIM 0.188792 0.146210 0.101382
    FASTSSIM 0.169534 0.033864 0.008820

--- src/state.c
+++ src/state.c
@@ -537,8 +537,9 @@ void od_adapt_ctx_reset(od_adapt_ctx *state, int is_keyframe) {
   od_adapt_pvq_ctx_reset(&state->pvq, is_keyframe);
   generic_model_init(&state->mv_model);
   OD_CDFS_INIT(state->mv_ref_cdf, 128);
-  state->skip_increment = 128;
-  OD_CDFS_INIT(state->skip_cdf, state->skip_increment >> 2);
+  state->skip_rate = is_keyframe ? 4 : 7;
+  OD_CLEAR(state->skip_count, OD_NBSIZES*2);
+  OD_CDFS_INIT(state->skip_cdf, 32);
   state->mv_small_increment = 128;
   OD_CDFS_INIT_FIRST(state->mv_small_cdf, state->mv_small_increment,
    10*state->mv_small_increment);
